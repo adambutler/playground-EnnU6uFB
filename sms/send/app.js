d746d7ba26fcc5cf62d1435c1df2aee8bf312b6b
Adam Butler <adam@lab.io>
2017-10-10 10:47:06 +0100

    Add API_KEY & API_SECRET

--- sms/send/app.js
+++ sms/send/app.js
@@ -1,12 +1,18 @@
-var Nexmo = require('nexmo')
+const Nexmo = require('nexmo')
 
-var nexmo = new Nexmo({
+const nexmo = new Nexmo({
   apiKey: 'API_KEY',
   apiSecret: 'API_SECRET'
 })
 
-var from = 'Nexmo'
-var to = 'TO_NUMBER'
-var text = 'A text message sent using the Nexmo SMS API'
+const from = 'Nexmo'
+const to = 'TO_NUMBER'
+const text = 'A text message sent using the Nexmo SMS API'
 
-nexmo.message.sendSms(from, to, text)
+nexmo.message.sendSms(from, to, text, (error, response) => {
+  if(error) {
+    console.log(error)
+  } else {
+    console.log(JSON.stringify(response, null, 2))
+  }
+})
