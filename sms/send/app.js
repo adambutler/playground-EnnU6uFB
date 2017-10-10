78f2a4bd68e1ae7f5d9d48d81a9c605e86656f1c
Adam Butler <adam@lab.io>
2017-10-10 15:25:52 +0100

    Add other tests for sms/send

--- sms/send/app.js
+++ sms/send/app.js
@@ -11,7 +11,10 @@ const text = 'A text message sent using the Nexmo SMS API'
 
 nexmo.message.sendSms(from, to, text, (error, response) => {
   if(error) {
-    console.log(error)
+    throw error
+  } else if(response.messages[0].status != '0') {
+    console.log(response)
+    throw 'Nexmo returned back a non-zero status'
   } else {
     console.log(JSON.stringify(response, null, 2))
   }
--- sms/send/app.spec.js
+++ sms/send/app.spec.js
@@ -2,13 +2,43 @@
 const rewire = require('rewire')
 const app = rewire('./app.js')
 
-it('should have data', function () {
+it('should have credentials', function () {
   try {
     const nexmo = app.__get__('nexmo')
     assert.notEqual(nexmo.credentials.apiKey, 'API_KEY')
     assert.notEqual(nexmo.credentials.apiKey, 'API_SECRET')
   } catch (error) {
     printMessage('Hint 💡', 'You need to set the API_KEY and API_SECRET')
+    throw error
+  }
+})
+
+it('should have a to number', function () {
+  try {
+    const to = app.__get__('to')
+    assert.notEqual(to, 'TO_NUMBER')
+  } catch (error) {
+    printMessage('Hint 💡', 'You need to set the TO_NUMBER. You should use your own number for testing.')
+    throw error
+  }
+})
+
+it('should have a valid to number', function () {
+  try {
+    const to = app.__get__('to')
+    assert.ok(/^[1-9]\d{1,14}$/.test(to))
+  } catch (error) {
+    printMessage('Hint 💡', 'It looks like the number you are sending to is not valid!')
+    throw error
+  }
+})
+
+it('should have text', function () {
+  try {
+    const to = app.__get__('to')
+    assert.notEqual(text, '')
+  } catch (error) {
+    printMessage('Hint 💡', 'You should provide some text.')
     throw error
   }
 })
